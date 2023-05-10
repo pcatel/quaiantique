c7ccae58883dc666b635c6bd20862605922e7a97
pcatel <pcatel@pascalcatel.com>
2023-05-10 16:33:46 +0200

    Finalisation maquette

--- reservation.php
+++ reservation.php
@@ -13,6 +13,7 @@
 
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/slider.css">
+  <link rel="stylesheet" href="css/reservation.css">
 
 
 </head>
@@ -50,28 +51,81 @@
     <div class="main">
 
 
-      <h2>Réservations</h2>
+      <h2>Réservation</h2>
 
       <div class="fakeimg">
 
         <p>Merci d'avance de préciser en commentaire le nombre d'enfants présent à table (si il y en a).</p>
-        <p>Pour tout retrad de plus de 20 mn votre table sera donnée à d'autre client !!!</p>
+        <p>Pour tout retard de plus de 20 mn votre table sera donnée à d'autre client !!!</p>
         <br>
 
-        <form action="#">
 
-          <label for=""><img src="img/icone/people-fill.svg"></label>
+        <form class="contact-form">
+          <label for="">Date souhaitée</label>
+          <input type="date" name="bday" required placeholder="dd-mm-yyyy" value=""
+        min="1997-01-01" max="2030-12-31" />
+
+
+          <label for="nbreCouverts">Nbre de couverts :</label>
           <select name="languages" id="lang">
-            <option value="2">2 couverts</option>
-            <option value="3">3 couverts</option>
-            <option value="4">4 couverts</option>
-            <option value="5">5 couverts</option>
-            <option value="6">6 couverts</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
             <option value="plusieurs">Plus de 6 couverts</option>
           </select>
-          <label for=""><img src="img/icone/calendar-date.svg"></label>
-          <input type="date" name="bday" required pattern="\d{4}-\d{2}-\d{2}" />
+
+          <label for="civilité">Civilité :</label>
+          <select name="languages" id="lang">
+            <option value="2">Mme</option>
+            <option value="3">Mlle</option>
+            <option value="2">Mr</option>
+          </select>
+
+
+
+
+          <label for="nom">Nom :</label>
+          <input type="text" id="nom" name="nom" required>
+
+          <label for="prenom">Prénom :</label>
+          <input type="text" id="prenom" name="prenom" required>
+
+          <label for="tel">Téléphone :</label>
+          <input type="tel" id="tel" name="tel" required>
+
+          <label for="email">Email :</label>
+          <input type="email" id="email" name="email" required>
+
+          <label for="couverts">Nombre de couverts :</label>
+          <input type="number" id="couverts" name="couverts" required>
+
+          <label for="story">Commentaires / Allergies:</label>
+
+          <textarea id="story" name="story" rows="5" cols="33">
+Précisez les éventuels allergies et si des enfants seront présents...
+</textarea>
+
+
+          <button type="submit">Envoyer</button>
         </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
