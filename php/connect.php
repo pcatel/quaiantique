427b9baa7c0a394fa97947d8f87e164ec55b6173
CATEL <pcatel@pascalcatel.com>
2023-07-19 10:31:57 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "91.216.107.182";
-$username = "pasca8966";
-$password = "gold1955";
-$dbname = "pasca8966_4gp9c9";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 ?>
